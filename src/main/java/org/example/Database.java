dd062b601cb9552f5d94f9595fe1df8c59043233
Amanda Gabriele Alves Ferreira <amanda@amandas-mbp.home>
2024-08-23 15:19:23 +0100

    feat: incomplete task

--- src/main/java/org/example/Database.java
+++ src/main/java/org/example/Database.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.db.DbConfig;
+import org.example.recipeClasses.*;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import static java.lang.Boolean.TRUE;
@@ -25,7 +26,26 @@ public class Database {
                 .setProperty(FORMAT_SQL, TRUE.toString())
                 .setProperty(HIGHLIGHT_SQL, TRUE.toString())
                 .setProperty("hibernate.agroal.maxSize", "20")
+                .addAnnotatedClass(Recipe.class)
+                .addAnnotatedClass(Ingredient.class)
+                .addAnnotatedClass(Category.class)
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Comment.class)
+                .addAnnotatedClass(Tag.class)
+                .addAnnotatedClass(Image.class)
+                .addAnnotatedClass(Rating.class)
                 .buildSessionFactory();
         return sessionFactory;
+    }
+
+    public static void seed(){
+        var sessionFactory = getSessionFactory();
+        // TODO
+        sessionFactory.inTransaction(session -> {
+            session.persist(new Ingredient(1, "Carrot", 500, "grams"));
+            session.flush();
+
+//            session.persist(new Recipe(/* your constructor params go here */));
+        });
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import org.hibernate.SessionFactory;
 
 public class Main {
     public static void main(String[] args) {
+        Database.seed();
         try (SessionFactory sessionFactory = Database.getSessionFactory()) {
             sessionFactory.getSchemaManager().exportMappedObjects(true);
         }
